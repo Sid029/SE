33ccf9f2529a0cbb660dab90f5105bf4cb8c3d92
Sid029 <91461689+Sid029@users.noreply.github.com>
2022-05-03 17:29:07 +0200

    Delete se-sprint-4 directory

--- se-sprint-4/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dbhost = 'localhost';
-    $user = 'seteam10';
-    $pass = 'QA87uEC2';
-    $db = 'seteam10';
-
-    
-    $conn = mysqli_connect('localhost', $user, $pass, $db);
-?>
--- se-sprint-4/logout_h.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['user_hospital']);
-    session_destroy();
-    header('Location: login_h.php');
-?>
